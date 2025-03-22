0cddb458e573cb31b75c5dc9e67b70ce8d3927d5
kellanburns <129882265+kellanburns@users.noreply.github.com>
2025-03-21 23:22:52 -0700

    Finish classes, add utilities

--- Utilities/Dijkstra.py
+++ Utilities/Dijkstra.py
@@ -1,2 +1,54 @@
 # Kellan Burns, python-Dijkstra project
+from Classes.LinkedList import LinkedList
+from Classes.MinHeap import MinHeap
 
+def reconstructPath(parents: [int], target: int) -> str:
+    path = []
+    while target != -1:
+        path.insert(0, str(target))
+        target = parents[target]
+    return ",".join(path)
+
+def printResults(distances: [int], parents: [int], source: int) -> None:
+    for i in range(len(distances)):
+        if (distances[i] == float('inf')):
+            print(f"[{i}] unreachable")
+        else:
+            path = reconstructPath(parents, i)
+            print(f"[{i}] shortest path: {path} shortest distance: {distances[i]}")
+
+def dijkstra(graph: [LinkedList], source: int) -> None:
+    n = graph.length
+    distances = [int]
+    parents = [int]
+    visited = [bool]
+    heap = MinHeap(n)
+
+    for i in range(n):
+        distances[i] = float('inf')
+        parents[i] = -1
+        heap.insert(i, distances[i])
+
+    distances[source] = 0
+    heap.decreaseKey(source, 0)
+
+    while heap:
+        u = heap.extractMin()
+        if (visited[u]):
+            continue
+        visited[u] = True
+
+        current = graph[u].head
+        while (current):
+            edge = current.edge
+            v = edge.destination
+            weight = edge.weight
+
+            if(not visited[v] and distances[u] + weight < distances[v]):
+                distances[v] = distances[u] + weight
+                parents[v] = u
+                heap.decreaseKey(v, distances[v])
+
+            current = current.next
+
+    printResults(distances, parents, source)
